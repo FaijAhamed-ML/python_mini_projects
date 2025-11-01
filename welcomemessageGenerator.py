3f474a8be8ae24424e580e4db41ecbf2946654d1
Faij Ahamed <ilamkavifaijahmed@gmail.com>
2025-11-02 00:33:19 +0530

    project 02 uploaded

---
+++ welcomemessageGenerator.py
@@ -0,0 +1,12 @@
+#welcome message generator
+
+#get input from user
+name = input("what's your name? ")
+hobby = input("what's your favorite hobby? ")
+
+#generate a personalized welcome message
+print("\n--- welcome Message ---")
+print(f"hello, {name}!")
+print("welcome to the world of python programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"Get ready to buildn something amazing totay.")
