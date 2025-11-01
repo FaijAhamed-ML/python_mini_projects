bfaf5572b43c7bb5403e909fae7151b4dd08b652
Faij Ahamed <ilamkavifaijahmed@gmail.com>
2025-11-02 00:58:56 +0530

    Add files via upload

---
+++ personalizedGreatingProgram.py
@@ -0,0 +1,12 @@
+# Personalized Greeting Program
+
+name = input("Enter your name: ")
+age = int(input("How old are you? "))
+color =input("what is your favorite color? ")
+print("+--------------------------------+")
+print("|      Personalized Greating      |")
+print("+--------------------------------+")
+
+print(f"\nhello, {name }! hi")
+print(f"\nyou are {age} years old and {color} is a beautiful color!")
+print("\nYou're now ready to start your python adventure ")
